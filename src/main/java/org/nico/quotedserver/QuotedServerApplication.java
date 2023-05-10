43f54ed72ef5e62cc8d5b422c07900767daaf73b
Nico <nico.pfau@icloud.com>
2023-05-10 22:57:59 +0200

    Initial commit

---
+++ src/main/java/org/nico/quotedserver/QuotedServerApplication.java
@@ -0,0 +1,13 @@
+package org.nico.quotedserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuotedServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuotedServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/nico/quotedserver/QuotedServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nico.quotedserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuotedServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
