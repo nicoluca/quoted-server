d7268d44bb6fa705149413478800e4325ac07e4b
Nico <nico.pfau@icloud.com>
2023-05-12 19:11:27 +0200

    Domain classes and first repository functional.

--- src/main/java/org/nico/quotedserver/QuotedServerApplication.java
+++ src/main/java/org/nico/quotedserver/QuotedServerApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class QuotedServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(QuotedServerApplication.class, args);
 	}
--- src/main/java/org/nico/quotedserver/repository/ArticleRepository.java
+++ src/main/java/org/nico/quotedserver/repository/ArticleRepository.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class ArticleRepository extends AbstractRepository<Article> {
+public class ArticleRepository extends AbstractRepository<Article> { // TODO replace with JPARepository<Article, Integer>
     @Autowired
     public ArticleRepository(EntityManager entityManager) {
         super(Article.class, entityManager);
